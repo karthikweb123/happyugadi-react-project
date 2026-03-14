1ab685e1cc9cf3462bd41c77263cf307b2e37c7d
karthikweb123 <karthikneelaram@gmail.com>
2026-03-14 23:18:57 +0530

    updated content

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ export default function App() {
     <div style={{padding:"40px", fontFamily:"Arial"}}>
       <h1>Happy Ugadi 🌸</h1>
       <p>Enter your name to get Ugadi wishes</p>
+      <h3>Have a wonderful bobbatlu!</h3>
 
       <UgadiForm />
     </div>
